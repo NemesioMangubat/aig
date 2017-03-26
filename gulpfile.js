036a29c47423156246479728b22261734ce971cd
Joshua Mark Jumalon <joshuajumalon82@gmail.com>
2017-03-26 21:32:26 +0800

    Added Major Work Flow

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,32 @@ gulp.task('css', function(){
 	.pipe(sourcemaps.init())
 	.pipe(sass({
 		outputStyle: 'expanded', 
+		sourceComments: true,
 		indentType: 'tab',
 		indentWidth: '1'
 	}).on('error', sass.logError))
 	.pipe(postcss([
-		autoprefixer('last 2 versions', '> 1%')
+		//autoprefixer('last 2 versions', '> 1%')
+		autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')
 	]))
 	.pipe(sourcemaps.write('maps'))
 	.pipe(gulp.dest('builds/development/assets/css'))
+});
+
+var sassBootstrapSources = ['components/sass-bootstrap/bootstrap.scss'];
+
+gulp.task('css-bootstrap', function(){
+	gulp.src(sassBootstrapSources)
+	//.pipe(sourcemaps.init())
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}).on('error', sass.logError))
+	.pipe(postcss([
+		//autoprefixer('last 2 versions', '> 1%')
+		autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')
+	]))
+	//.pipe(sourcemaps.write('maps'))
+	.pipe(gulp.dest('builds/development/assets/plugins/bootstrap/css'))
 });
 
 gulp.task('watch', function(){
@@ -34,6 +52,7 @@ gulp.task('watch', function(){
         server: 'builds/development'
     });
 	gulp.watch(['components/sass/*.scss'], ['css']);
+	gulp.watch(['components/sass-bootstrap/*.scss'] ,['css-bootstrap']);
 	gulp.watch('builds/development/**').on('change', browserSync.reload);
 });
 
